17c19f76aba96339e4ea7589bdbd2a653ef1b2f0
Your Name <you@example.com>
2025-06-21 10:45:18 +0000

    dd

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
+str1 = "Holberton"  # variable str1
+str2 = "School"     # variable str2
 # Using str1 and str2 to build the welcome message
-print(f"Welcome to {str1} {str2}!")
+print("Welcome to " + str1 + " " + str2 + "!")
